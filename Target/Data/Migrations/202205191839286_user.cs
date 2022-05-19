a4457a6baea50e85570f599f32cc037a14fb60ef
Йордан Йорданов <dani_yordani@abv.bg>
2022-05-19 21:40:48 +0300

    Data layer implemented

---
+++ Target/Data/Migrations/202205191839286_user.cs
@@ -0,0 +1,31 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class user : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        CreatedBy = c.Int(nullable: false),
+                        CreatedOn = c.DateTime(),
+                        UpdatedBy = c.Int(nullable: false),
+                        UpdatedOn = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
