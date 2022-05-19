da91331dcf06a752eb5ce7cd12782d4922e7f250
Йордан Йорданов <dani_yordani@abv.bg>
2022-05-19 21:49:47 +0300

    Repository create

---
+++ Target/Repository/Implementations/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using Data.Context;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementations
+{
+    public class UnitOfWork : IDisposable
+    {
+        private DataContext context = new DataContext();
+        private GenericRepository<User> userRepository;
+
+        public GenericRepository<User> DepartmentRepository
+        {
+            get
+            {
+
+                if (this.userRepository == null)
+                {
+                    this.userRepository = new GenericRepository<User>(context);
+                }
+                return userRepository;
+            }
+        }
+
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
