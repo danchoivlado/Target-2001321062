e2558465fd17d2c4746f7641cca3e73e4a594077
Йордан Йорданов <dani_yordani@abv.bg>
2022-05-27 17:44:58 +0300

    Added MVC project with index and create functionallity

---
+++ Target/MVC/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationService.Implementaions;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models.Users;
+using System.Net;
+
+namespace MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManagementService userManagementService = new UserManagementService();
+
+        public IActionResult Index()
+        {
+            List<UserIndexViewModel> usersVM = new List<UserIndexViewModel>();
+            foreach (var user in userManagementService.Get())
+            {
+                usersVM.Add(new UserIndexViewModel(user));
+            }
+
+
+            return View(usersVM);
+        }
+
+        //[Route("Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var user = userManagementService.GetById(id);
+
+            return View(new UserIndexViewModel(user));
+        }
+    }
+}
