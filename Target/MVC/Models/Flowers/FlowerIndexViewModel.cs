32734f7886a053492860cb20639a049162adeda9
esterrrr <73944811+esterrrr@users.noreply.github.com>
2022-05-31 01:03:43 +0300

    Create flower crud operations

---
+++ Target/MVC/Models/Flowers/FlowerIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationService.DTOs;
+
+namespace MVC.Models.Flowers
+{
+    public class FlowerIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public FlowerIndexViewModel(FlowerDTO flowerDTO)
+        {
+            this.Id = flowerDTO.Id;
+            this.Name = flowerDTO.Name;
+            this.Price = flowerDTO.Price;
+        }
+        public FlowerIndexViewModel()
+        {
+
+        }
+    }
+}
