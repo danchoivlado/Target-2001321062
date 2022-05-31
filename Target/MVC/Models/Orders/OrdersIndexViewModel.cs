a7d91665a75913848a7a3c11e65297c2cc8e4c85
esterrrr <73944811+esterrrr@users.noreply.github.com>
2022-06-01 02:26:52 +0300

    Add Order index page in MVC project

--- Target/MVC/Models/Orders/OrdersIndexViewModel.cs
+++ Target/MVC/Models/Orders/OrdersIndexViewModel.cs
@@ -5,10 +5,12 @@ namespace MVC.Models.Orders
     public class OrdersIndexViewModel
     {
         public int Id { get; set; }
+        public string UserUserName { get; set; }
 
         public OrdersIndexViewModel(OrderDTO orderDTO)
         {
             this.Id = orderDTO.Id;
+            this.UserUserName = orderDTO.User.UserName;
         }
         public OrdersIndexViewModel()
         {
