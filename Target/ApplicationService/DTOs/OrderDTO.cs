19b093a167093c283bacf8ed1506a057ad4d80b1
esterrrr <73944811+esterrrr@users.noreply.github.com>
2022-05-31 16:14:06 +0300

    Create OrderManagementService

--- Target/ApplicationService/DTOs/OrderDTO.cs
+++ Target/ApplicationService/DTOs/OrderDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace ApplicationService.DTOs
 {
     internal class OrderDTO
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
--- Target/ApplicationService/Implementaions/OrderManagementService.cs
+++ Target/ApplicationService/Implementaions/OrderManagementService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ApplicationService.DTOs;
+using Data.Entities;
+using Repository.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,103 @@ namespace ApplicationService.Implementaions
 {
     internal class OrderManagementService
     {
+        public List<OrderDTO> Get()
+        {
+            List<OrderDTO> ordersDto = new List<OrderDTO>();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                foreach (var item in unitOfWork.OrderRepository.Get())
+                {
+
+                    ordersDto.Add(new OrderDTO
+                    {
+                        Id = item.Id,
+                        UserId = item.UserId,
+                    });
+
+                }
+            }
+            return ordersDto;
+        }
+        public OrderDTO GetById(int id)
+        {
+            OrderDTO ordersDTO = new OrderDTO();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Order order = unitOfWork.OrderRepository.GetByID(id);
+                if (order != null)
+                {ordersDTO = new OrderDTO
+                {
+                        Id = order.Id,
+                        UserId = order.UserId,
+                };
+                }
+            }
+
+            return ordersDTO;
+        }
+        public bool Save(OrderDTO ordersDTO)
+        {
+            Order order = new Order()
+            {
+                UserId = ordersDTO.UserId,
+            };
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    unitOfWork.OrderRepository.Insert(order);
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    Order orders = unitOfWork.OrderRepository.GetByID(id);
+                    unitOfWork.OrderRepository.Delete(orders);
+                    unitOfWork.Save();
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Update(OrderDTO orderDto)
+        {
+            Order order = new Order()
+            {
+                Id = orderDto.Id,
+                UserId=orderDto.UserId,
+            };
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    unitOfWork.OrderRepository.Update(order);
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
