c520b1f3794db4a0274093518e55cee0d6a5f244
esterrrr <73944811+esterrrr@users.noreply.github.com>
2022-06-01 12:07:43 +0300

    Crete OrderItem table

--- Target/Data/Entities/Order.cs
+++ Target/Data/Entities/Order.cs
@@ -8,7 +8,12 @@ namespace Data.Entities
 {
     public class Order : BaseEntity
     {
+        public Order()
+        {
+            this.OrderItems = new HashSet<OrderItem>();
+        }
         public int UserId { get; set; }
         public virtual User User { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
     }
 }
